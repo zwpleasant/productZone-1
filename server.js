8e355e1b1f960b58651e10f734acde165da35781
Mark Thompson <mark.thompson19@gmail.com>
2018-04-03 18:04:42 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var exprhbs = require("express-handlebars");
+
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exprhbs({defaultLayout: main}));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});

55fbaab2b8d14723d3a837a429aa0955f2e7bca9
Mark Thompson <mark.thompson19@gmail.com>
2018-04-03 18:16:08 -0500

    Import Router

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var exprhbs = require("express-handlebars");
+var htmlRouter = require("./routes/htmlRoutes");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -10,6 +11,8 @@ app.use(bodyParser.json());
 
 app.engine("handlebars", exprhbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
+
+app.use(htmlRouter);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
